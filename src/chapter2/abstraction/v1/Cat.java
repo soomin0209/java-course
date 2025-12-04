b508b9e2f5d26ccaff4b407d26d8f5073b87501f
soomin0209 <soomin02094@gmail.com>
2025-12-04 21:40:57 +0900

    refactor: reorder methods in Cat.java

--- src/chapter2/abstraction/v1/Cat.java
+++ src/chapter2/abstraction/v1/Cat.java
@@ -2,12 +2,12 @@ package chapter2.abstraction.v1;
 
 public class Cat implements Animal{
     @Override
-    public void makeSound() {
+    public void exist() {
         System.out.println("고양이가 존재합니다.");
     }
 
     @Override
-    public void exist() {
+    public void makeSound() {
         System.out.println("야옹");
     }
 
